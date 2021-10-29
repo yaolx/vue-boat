1dd69389ca96d3cc961d01dbcc351226abdc617e
yaolx <644894056@qq.com>
2021-10-30 06:59:20 +0800

    feature: vite.config.js引入插件

--- config/vite/plugins/index.ts
+++ config/vite/plugins/index.ts
@@ -3,30 +3,29 @@
  */
 
 import { Plugin } from 'vite'
-/* import viteCompression from 'vite-plugin-compression'
+
 import legacy from '@vitejs/plugin-legacy'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import viteCompression from 'vite-plugin-compression'
 import eslint from '@rollup/plugin-eslint'
-import typescript from '@rollup/plugin-typescript' */
+import typescript from '@rollup/plugin-typescript' 
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 import { VITE_APP_ANALYZE, VITE_APP_COMPRESS_GZIP } from '../../constant'
 import configMockPlugin from './mock'
-// import configVisualizerPlugin from './visualizer'
+import configVisualizerPlugin from './visualizer'
 // import configStyleImportPlugin from './styleImport'
 
 export function createVitePlugins(viteEnv: string, isBuild: boolean) {
   const vitePlugins: (Plugin | Plugin[])[] = [
-    // {
-    //   ...eslint({
-    //     include: 'src/**/*.+(js|jsx|ts|tsx)'
-    //   }),
-    //   enforce: 'pre'
-    // },
-    // typescript(),
-    // reactRefresh(),
-    // legacy(),
+    {
+      ...eslint({
+        include: 'src/**/*.+(js|jsx|ts|tsx|vue)'
+      }),
+      enforce: 'pre'
+    },
+    typescript(),
+    legacy(),
     vue(),
     vueJsx()
     // configStyleImportPlugin()
@@ -36,14 +35,14 @@ export function createVitePlugins(viteEnv: string, isBuild: boolean) {
   viteEnv === 'mock' && vitePlugins.push(configMockPlugin(isBuild))
 
   // 包分析
-  // VITE_APP_ANALYZE && vitePlugins.push(configVisualizerPlugin())
+  VITE_APP_ANALYZE && vitePlugins.push(configVisualizerPlugin())
 
   // 发布，打包
-  // if (VITE_APP_COMPRESS_GZIP && isBuild) {
-  //   vitePlugins.push(
-  //     viteCompression({ deleteOriginFile: true })
-  //   )
-  // }
+  if (VITE_APP_COMPRESS_GZIP && isBuild) {
+    vitePlugins.push(
+      viteCompression({ deleteOriginFile: true })
+    )
+  }
 
   return vitePlugins
 }
