6e81d1dd6ef5f0d21b3e84297764286e6186afdf
yaolx <644894056@qq.com>
2021-11-02 23:05:17 +0800

    feature: 引入ant-design-vue、vuex

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import Antd from 'ant-design-vue'
+import App from '@/App.vue'
 import routers from '@/routes'
+import store from '@/store/index'
+import 'ant-design-vue/dist/antd.css'
 import './index.css'
 
-createApp(App).use(routers).mount('#app')
+
+const app = createApp(App)
+app
+.use(routers)
+.use(store)
+.use(Antd)
+.mount('#app')
